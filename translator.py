c8c6edcefa235977a7bcdd3765a676a6e6019936
CHENTECHTOP <140854935+CHENTECHTOP@users.noreply.github.com>
2024-11-29 21:14:06 +0200

    Перекладач

--- translator.py
+++ translator.py
@@ -59,7 +59,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Перекладач"))
+        Form.setWindowTitle(_translate("Form", "Vladichhok Перекладач"))
         self.textBrowser.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
